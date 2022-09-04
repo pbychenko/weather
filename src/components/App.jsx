cd5a951d806456f4282349ad8454542581354b6d
pavelv <pavelv87@gmail.com>
2022-09-04 10:49:06 +0600

    fix changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,51 +2,29 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import Alert from 'react-bootstrap/Alert';
 import CityInput from './CityInput.jsx';
 import TodayCard from './TodayCard.jsx';
 import ForeCast from './ForeCast.jsx';
 
-
-const baseUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=43.25&lon=76.95&units=metric&appid=${token}`;
-
-// const cities = ['almaty', 'moscow', 'minsk', 'kiev'];
+const token = '260023c9f57441c591cecb63eb4bde59';
 
 const App = () => {
   const [current, setCurrent] = useState(null);
   const [daily, setDaily] = useState([]);
-  const [coords, setCoords] = useState({ lat: '', lon: '' });
-  // const [activePictureData, setActivePictureData] = useState(null);
-  // const [showModal, setShowModal] = useState(false);
-  // const [showErrorBlock, setShowErrorBlock] = useState(false);
+  const [errorBlock, setErrorBlock] = useState('');
   const [form, setForm] = useState({ city: '' });
 
-  const getDataRequest = async () => {
-    try {
-      const res = await axios.get(baseUrl);
-      setCurrent(res.data.current);
-      setDaily(res.data.daily);
-    } catch (error) {
-      // setShowErrorBlock(true);
-      console.log(error);
-      throw error;
-    }
+  const getDataRequest = async (lat, lon) => {
+    const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${token}`;
+    const res = await axios.get(url);
+    setCurrent(res.data.current);
+    setDaily(res.data.daily);
   };
 
-  // const openCard = async (id) => {
-  //   const uri = baseUrl + (id ? `/${id}` : '');
-  //   try {
-  //     const res = await axios.get(uri);
-  //     setActivePictureData(res.data);
-  //     setShowModal(true);
-  //   } catch (error) {
-  //     setShowErrorBlock(true);
-  //     throw error;
-  //   }
-  // };
-
-  useEffect(() => {
-    getDataRequest();
-  }, []);
+  // useEffect(() => {
+  //   getDataRequest();
+  // }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -56,49 +34,25 @@ const App = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { city } = form;
-    const baseUrl2 = `http://api.openweathermap.org/geo/1.0/direct?q=${city}ы&appid=${token}`;
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${token}`;
     try {
-      const res = await axios.get(baseUrl2);
-      console.log(res)
-      // setCurrent(res.data.current);
-      // setDaily(res.data.daily);
-      setForm({ city: '' });
-      // setShowModal(false);
+      const resposeData = (await axios.get(url)).data[0];
+      if (resposeData) {
+        const { lat, lon } = resposeData;
+        await getDataRequest(lat, lon);
+        setForm({ city: '' });
+      } else {
+        setErrorBlock('incorrect city');
+        // setCurrent(null);
+        // setDaily([]);
+      }
     } catch (error) {
-      console.log(error);
-      // setShowErrorBlock(true);
+      setErrorBlock('Something wrong with newtwork please try later');
       throw error;
     }
   };
-
-  // const handleCloseModal = () => {
-  //   setShowModal(false);
-  //   setForm({ name: '', comment: '' });
-  // };
-
-  // const renderModal = () => (
-  //   activePictureData && <MyModal
-  //     show={showModal}
-  //     data={activePictureData}
-  //     onFormChange={handleChange}
-  //     onFormSubmit={handleSubmit}
-  //     formData={form}
-  //     onHide={handleCloseModal}
-  //   />
-  // );
-  // console.log('requestState');
-
-  // if (showErrorBlock) {
-  //   return (
-  //     <Alert variant='info' className="text-center">
-  //       Something wrong with newtwork please try later
-  //     </Alert>
-  //   );
-  // }
-  // const [key, setKey] = useState('today');
-
-  // console.log(current);
-  // console.log(daily);
+  
+  console.log('test')
 
   return (
     <>
@@ -107,6 +61,10 @@ const App = () => {
           onFormSubmit={handleSubmit}
           formData={form}
         />
+        {errorBlock !== '' ? (
+        <Alert variant='info' className="text-center">
+          {errorBlock}
+        </Alert>) : null}
         {current !== null ? (<TodayCard data={current}/>) : null}
         {daily !== [] ? (<ForeCast data={daily}/>) : null}
       </Container>
