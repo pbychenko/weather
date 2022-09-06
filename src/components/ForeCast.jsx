77607416665eb8ce8691dd892d680e4458806ec6
pavelv <pavelv87@gmail.com>
2022-09-06 15:27:44 +0600

    final

--- src/components/ForeCast.jsx
+++ src/components/ForeCast.jsx
@@ -6,7 +6,7 @@ import ForecastCard from './ForecastCard.jsx';
 
 const ForeCast = ({ data }) => (
   <>
-    <h1 className='text-center'>FORECAST without package.json and server.js as server</h1>
+    <h1 className='text-center'>FORECAST</h1>
     <Container>
       <Row className="align-items-center">
         {data.map((dayData) => <Col key={dayData.dt} sm={3} className='mb-3'><ForecastCard dayData={dayData} /></Col>)}
